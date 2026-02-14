5875bfe2fa2da13cc4e80ecd6444cf9d6dae53e0
Stalemated <52725146+Stalemated@users.noreply.github.com>
2026-02-14 11:50:08 -0600

    Add config file to toggle different armor tweaks

---
+++ src/main/java/com/stalemated/MutantSkeletonArmorConfig.java
@@ -0,0 +1,21 @@
+package com.stalemated;
+
+import com.stalemated.config.ConfigManager;
+
+public class MutantSkeletonArmorConfig {
+    public static boolean enableSkullMultishot;
+    public static boolean enableChestplateDrawSpeed;
+    public static boolean enableChestplateCrossbowTweak;
+    public static boolean enableLeggingsEffect;
+    public static boolean enableBootsEffect;
+
+    public static void load() {
+        ConfigManager config = new ConfigManager("mutant_skeleton_armor_tweaks.json");
+
+        enableSkullMultishot = config.getOrDefault("enable_skull_multishot", true);
+        enableChestplateDrawSpeed = config.getOrDefault("enable_chestplate_draw_speed_buff", true);
+        enableChestplateCrossbowTweak = config.getOrDefault("enable_chestplate_crossbow_tweak", true);
+        enableLeggingsEffect = config.getOrDefault("enable_leggings_effect", true);
+        enableBootsEffect = config.getOrDefault("enable_boots_effect", true);
+    }
+}
