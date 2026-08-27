0adc2b03ce4c9331e73ddf0a4c8de3967cbe3cb3
Stalemated <52725146+Stalemated@users.noreply.github.com>
2026-08-26 20:41:42 -0600

    Feat: registered the config screen for Forge

--- forge/src/main/java/com/stalemated/mutantskeletweaks/forge/client/MSATForgeClient.java
+++ forge/src/main/java/com/stalemated/mutantskeletweaks/forge/client/MSATForgeClient.java
@@ -1,13 +1,19 @@
 package com.stalemated.mutantskeletweaks.forge.client;
 
 import com.stalemated.mutantskeletweaks.client.MSATClient;
+import com.stalemated.mutantskeletweaks.gui.screen.MSATConfigScreen;
+import net.minecraftforge.client.ConfigScreenHandler;
 import net.minecraftforge.client.event.ClientPlayerNetworkEvent;
 import net.minecraftforge.common.MinecraftForge;
 import com.stalemated.mutantskeletweaks.network.MSATNetworkHandler;
+import net.minecraftforge.fml.ModLoadingContext;
 
+@SuppressWarnings("removal")
 public final class MSATForgeClient {
     public static void init() {
         MSATClient.init();
+        ModLoadingContext.get().registerExtensionPoint(ConfigScreenHandler.ConfigScreenFactory.class,
+                () -> new ConfigScreenHandler.ConfigScreenFactory((client, parent) -> MSATConfigScreen.create(parent)));
         MinecraftForge.EVENT_BUS.addListener(MSATForgeClient::onPlayerLogout);
     }
 
