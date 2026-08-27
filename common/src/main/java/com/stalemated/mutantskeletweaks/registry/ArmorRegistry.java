d5f3ca3147f1819c9f0de4fd310abb33517d7819
Stalemated <52725146+Stalemated@users.noreply.github.com>
2026-08-26 20:36:11 -0600

    Fix: fixed item id registration crashing forge instances, switching back to a classic string register

--- common/src/main/java/com/stalemated/mutantskeletweaks/registry/ArmorRegistry.java
+++ common/src/main/java/com/stalemated/mutantskeletweaks/registry/ArmorRegistry.java
@@ -2,16 +2,12 @@ package com.stalemated.mutantskeletweaks.registry;
 
 import fuzs.mutantmonsters.init.ModRegistry;
 import net.minecraft.item.Item;
-import net.minecraft.registry.Registries;
 
 public class ArmorRegistry {
     public static Item skullItem = ModRegistry.MUTANT_SKELETON_SKULL_ITEM.get();
-    public static Item chestItem = ModRegistry.MUTANT_SKELETON_CHESTPLATE_ITEM.get();
-    public static Item legsItem = ModRegistry.MUTANT_SKELETON_LEGGINGS_ITEM.get();
-    public static Item bootsItem = ModRegistry.MUTANT_SKELETON_BOOTS_ITEM.get();
 
-    public static String skullId = String.valueOf(Registries.ITEM.getId(skullItem));
-    public static String chestId = String.valueOf(Registries.ITEM.getId(chestItem));
-    public static String legsId = String.valueOf(Registries.ITEM.getId(legsItem));
-    public static String bootsId = String.valueOf(Registries.ITEM.getId(bootsItem));
+    public static final String skullId = "mutantmonsters:mutant_skeleton_skull";
+    public static final String chestId = "mutantmonsters:mutant_skeleton_chestplate";
+    public static final String legsId = "mutantmonsters:mutant_skeleton_leggings";
+    public static final String bootsId = "mutantmonsters:mutant_skeleton_boots";
 }
