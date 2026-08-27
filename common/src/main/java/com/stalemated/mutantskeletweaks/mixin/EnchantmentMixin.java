ce6b651dbe15fac9822d7f6c36b6bbb030b87482
Stalemated <52725146+Stalemated@users.noreply.github.com>
2026-08-26 20:23:16 -0600

    Fix: fixed the target not being shadowed from Enchantment class

--- common/src/main/java/com/stalemated/mutantskeletweaks/mixin/EnchantmentMixin.java
+++ common/src/main/java/com/stalemated/mutantskeletweaks/mixin/EnchantmentMixin.java
@@ -6,20 +6,20 @@ import net.minecraft.enchantment.EnchantmentTarget;
 import net.minecraft.item.ItemStack;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(Enchantment.class)
 public abstract class EnchantmentMixin {
-    @Unique
     @Final
-    public EnchantmentTarget msat$type;
+    @Shadow
+    public EnchantmentTarget target;
 
     @Inject(method = "isAcceptableItem", at = @At("HEAD"), cancellable = true)
     private void allowMutantSkullEnchantments(ItemStack stack, CallbackInfoReturnable<Boolean> cir) {
-        if (ArmorEnchantabilityHelper.enchantabilityConditions(msat$type, stack.getItem())) {
+        if (ArmorEnchantabilityHelper.enchantabilityConditions(target, stack.getItem())) {
             cir.setReturnValue(true);
         }
     }
